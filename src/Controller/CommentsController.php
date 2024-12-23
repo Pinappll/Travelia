df97e1ee640464933b4ced2d67d0792760dae988
Joshua Grandville <joshua77130@live.fr>
2024-12-23 02:32:43 +0100

    savve

--- src/Controller/CommentsController.php
+++ src/Controller/CommentsController.php
@@ -6,23 +6,31 @@ use App\Entity\Comment;
 use App\Form\Comment1Type;
 use App\Repository\CommentRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/admin/comments')]
+
 final class CommentsController extends AbstractController
 {
-    #[Route(name: 'app_comments_index', methods: ['GET'])]
-    public function index(CommentRepository $commentRepository): Response
+    #[Route('/admin/comments',name: 'app_comments_index', methods: ['GET'])]
+    public function index(CommentRepository $commentRepository, PaginatorInterface $paginator, Request $request): Response
     {
+        $query = $commentRepository->createQueryBuilder('c')->getQuery();
+        $pagination = $paginator->paginate(
+            $query,                                // Requête Doctrine
+            $request->query->getInt('page', 1),   // Numéro de la page actuelle
+            10                                    // Nombre d'éléments par page
+        );
+
         return $this->render('comments/index.html.twig', [
-            'comments' => $commentRepository->findAll(),
+            'pagination' => $pagination,
         ]);
     }
 
-    #[Route('/new', name: 'app_comments_new', methods: ['GET', 'POST'])]
+    #[Route('/admin/comments/new', name: 'app_comments_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $comment = new Comment();
@@ -42,7 +50,7 @@ final class CommentsController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_comments_show', methods: ['GET'])]
+    #[Route('/admin/comments/{id}', name: 'app_comments_show', methods: ['GET'])]
     public function show(Comment $comment): Response
     {
         return $this->render('comments/show.html.twig', [
@@ -50,7 +58,7 @@ final class CommentsController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_comments_edit', methods: ['GET', 'POST'])]
+    #[Route('/admin/comments/{id}/edit', name: 'app_comments_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Comment $comment, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(Comment1Type::class, $comment);
@@ -68,7 +76,7 @@ final class CommentsController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_comments_delete', methods: ['POST'])]
+    #[Route('/admin/comments/{id}', name: 'app_comments_delete', methods: ['POST'])]
     public function delete(Request $request, Comment $comment, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$comment->getId(), $request->getPayload()->getString('_token'))) {
@@ -78,4 +86,35 @@ final class CommentsController extends AbstractController
 
         return $this->redirectToRoute('app_comments_index', [], Response::HTTP_SEE_OTHER);
     }
+
+    #[Route('/my-comments', name: 'app_user_comments', methods: ['GET'])]
+    public function myComments(
+        CommentRepository $commentRepository,
+        PaginatorInterface $paginator,
+        Request $request
+    ): Response {
+        $user = $this->getUser();
+
+        if (!$user) {
+            // Redirige vers la page de connexion si l'utilisateur n'est pas connecté
+            return $this->redirectToRoute('app_login');
+        }
+
+        // Crée une requête pour récupérer les commentaires de l'utilisateur courant
+        $query = $commentRepository->createQueryBuilder('c')
+            ->where('c.publisher = :user')
+            ->setParameter('user', $user)
+            ->getQuery();
+
+        $pagination = $paginator->paginate(
+            $query,                                // Requête Doctrine
+            $request->query->getInt('page', 1),   // Numéro de la page actuelle
+            10                                    // Nombre d'éléments par page
+        );
+
+        return $this->render('comments/my_comments.html.twig', [
+            'pagination' => $pagination,
+        ]);
+    }
+
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -11,10 +11,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('admin/user')]
+
 final class UserController extends AbstractController
 {
-    #[Route(name: 'app_user_index', methods: ['GET'])]
+    #[Route('admin/user', name: 'app_user_index', methods: ['GET'])]
     public function index(UserRepository $userRepository): Response
     {
         return $this->render('user/index.html.twig', [
@@ -22,7 +22,7 @@ final class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
+    #[Route('admin/user/new', name: 'app_user_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
@@ -42,7 +42,7 @@ final class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
+    #[Route('admin/user/{id}', name: 'app_user_show', methods: ['GET'])]
     public function show(User $user): Response
     {
         return $this->render('user/show.html.twig', [
@@ -50,7 +50,7 @@ final class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
+    #[Route('admin/user/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(UserType::class, $user);
@@ -68,7 +68,7 @@ final class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
+    #[Route('admin/user/{id}', name: 'app_user_delete', methods: ['POST'])]
     public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->getPayload()->getString('_token'))) {
@@ -78,4 +78,34 @@ final class UserController extends AbstractController
 
         return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
     }
+    #[Route('/profile', name: 'app_user_profile', methods: ['GET', 'POST'])]
+    public function profile(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = $this->getUser(); // Récupère l'utilisateur actuellement connecté
+
+        if (!$user) {
+            // Redirection si l'utilisateur n'est pas connecté
+            return $this->redirectToRoute('app_login');
+        }
+
+        // Crée le formulaire avec les données de l'utilisateur
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Sauvegarde les modifications
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Votre profil a été mis à jour avec succès.');
+
+            // Redirection vers la page profil après mise à jour
+            return $this->redirectToRoute('app_user_profile');
+        }
+
+        return $this->render('user/edit.html.twig', [
+            'form' => $form->createView(),
+            'user' => $user,
+        ]);
+    }
+
 }
